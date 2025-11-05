77508e25b05cc5e3f89b836ce6d1e78f0e3228d2
Sito Shinnosuke <sitsnk@icloud.com>
2025-11-05 13:25:05 +0900

    test

---
+++ test.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SPDX-FileCopyrightText: 2025 Saito Shinnosuke
+#SPDX-License-Identifier: GPL-3.0-only\
+
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus_stdin)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
+exit $res
