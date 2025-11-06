2c2dcd81e26ca7195540b4eecd6ffe17e4fc9383
Sito Shinnosuke <sitsnk@icloud.com>
2025-11-06 15:51:45 +0900

    add test_weekday file

---
+++ test_weekday.bash
@@ -0,0 +1,10 @@
+#!/bin/bash -xv 
+#SPDX-FileCopyrightText: 2025 Saito Shinnosuke
+#SPDX-License-Identifier: GPL-3.0-only
+
+ng(){ echo "$1 行目が違うよ"; exit 1; }
+
+out="$(printf '%s\n' 2025-11-06 | ./weekday)"
+[ "$out" = "木" ] || ng "$LINENO"
+
+echo OK
