4bf80457895738fa0ef2e19b7fe3f5040c05e5d8
Sito Shinnosuke <sitsnk@icloud.com>
2025-11-06 16:10:41 +0900

    add test_weekday file

--- test_weekday.bash
+++ test_weekday.bash
@@ -7,4 +7,12 @@ ng(){ echo "$1 行目が違うよ"; exit 1; }
 out="$(printf '%s\n' 2025-11-06 | ./weekday)"
 [ "$out" = "木" ] || ng "$LINENO"
 
+out="$(printf '%s\n' 2028-02-29 | ./weekday)"
+[ "$out" = "火" ] || ng "$LINENO"
+
+out="$(printf '%s\n' 2025-11-06 2028-02-29 | ./weekday)"
+[ "$out" = $'木\n火' ] || ng "$LINENO"
+
+
+
 echo OK
